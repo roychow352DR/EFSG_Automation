00f752284ff6ec2058765b00adc53c3a1db7138e
Roy Chow <roychow@EGIT230316.local>
2026-02-12 16:06:34 +0800

    app

--- src/main/java/PageObject/NativeApp/AppTradeView.java
+++ src/main/java/PageObject/NativeApp/AppTradeView.java
@@ -8,21 +8,29 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import java.util.Arrays;
+import java.util.List;
 
 public class AppTradeView {
 
     private final AppiumDriver driver;
     private final MobileAbstractComponents abs;
+    public static String stopLossPrice;
+    public static String takeProfitPrice;
+    public static String selectedDirection;
+    public static String lotSize;
+    public static String executedPrice;
 
     public AppTradeView(AppiumDriver driver) {
         this.driver = driver;
         abs = new MobileAbstractComponents(driver);
-        PageFactory.initElements(driver,this);
+        PageFactory.initElements(driver, this);
     }
 
     @FindBy(xpath = "//android.widget.TextView[@text=\"BUY\"]/parent::android.view.ViewGroup")
     WebElement buyButtonAos;
+
+    @FindBy(xpath = "//android.widget.TextView[@text=\"SELL\"]/parent::android.view.ViewGroup")
+    WebElement sellButtonAos;
 
     @FindBy(xpath = "//android.widget.ScrollView/android.view.ViewGroup/android.widget.TextView[5]")
     WebElement stopLossPriceAos;
@@ -30,10 +38,33 @@ public class AppTradeView {
     @FindBy(xpath = "//android.widget.Switch")
     WebElement stopLossSwitchAos;
 
+    @FindBy(xpath = "//android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[10]/android.widget.EditText")
+    WebElement stopLossTextFieldAos;
+
+    @FindBy(xpath = "//android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[14]/android.widget.EditText")
+    WebElement takeProfitTextFieldAos;
+
+    @FindBy(className = "android.widget.TextView")
+    WebElement confirmationTextAos;
+
+    @FindBy(xpath = "//android.view.ViewGroup[@resource-id=\"RNE__Overlay\"]/android.view.ViewGroup[12]/android.view.ViewGroup")
+    WebElement closeButtonAos;
+
+    @FindBy(className = "android.widget.EditText")
+    WebElement lotSizeTextFieldAos;
+
+    @FindBy(xpath = "//android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.widget.TextView")
+    List<WebElement> rowsOnPositionTab;
+
+    @FindBy(xpath = "//android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.view.ViewGroup[3]/android.view.ViewGroup")
+    WebElement detailsButton;
+
     public void selectDirection(String direction) {
+        selectedDirection = direction;
         if (driver instanceof AndroidDriver) {
             switch (direction) {
                 case "BUY" -> buyButtonAos.click();
+                case "SELL" -> sellButtonAos.click();
             }
         }
     }
@@ -45,24 +76,140 @@ public class AppTradeView {
         return null;
     }
 
-    public void switchProfitStopLoss(){
+    public void switchProfitStopLoss() throws InterruptedException {
         if (driver instanceof AndroidDriver) {
+            Thread.sleep(2000);
             stopLossSwitchAos.click();
         }
     }
 
-    public String getStopLossPrice(){
-        String price;
-        WebElement text = driver.findElement(By.xpath("//android.widget.TextView[contains(@text,\"Stop Loss (≤\")]"));
-        price = text.getText().split("≤")[1].trim().split("\\)")[0];
+    public String getStopLossPrice(String direction) {
+        String price = "";
+        WebElement text;
+        selectedDirection = direction;
+        switch (selectedDirection) {
+            case "BUY" -> {
+                text = driver.findElement(By.xpath("//android.widget.TextView[contains(@text,\"Stop Loss (≤\")]"));
+                price = Float.toString(Float.parseFloat(text.getText().split("≤")[1].trim().split("\\)")[0]) - 100);
+            }
+            case "SELL" -> {
+                text = driver.findElement(By.xpath("//android.widget.TextView[contains(@text,\"Stop Loss (≥\")]"));
+                price = Float.toString(Float.parseFloat(text.getText().split("≥")[1].trim().split("\\)")[0]) + 100);
+            }
+        }
+        stopLossPrice = price;
         return price;
     }
 
-    public String getTakeProfitPrice(){
-        String price;
-        WebElement text = driver.findElement(By.xpath("//android.widget.TextView[contains(@text,\"Take Profit (≥\")]"));
-        price = text.getText().split("≤")[1].trim().split("\\)")[0];
+    public String getTakeProfitPrice(String direction) {
+        String price = "";
+        WebElement text;
+        selectedDirection = direction;
+        switch (selectedDirection) {
+            case "BUY" -> {
+                text = driver.findElement(By.xpath("//android.widget.TextView[contains(@text,\"Take Profit (≥\")]"));
+                price = Float.toString(Float.parseFloat(text.getText().split("≥")[1].trim().split("\\)")[0]) + 100);
+            }
+            case "SELL" -> {
+                text = driver.findElement(By.xpath("//android.widget.TextView[contains(@text,\"Take Profit (≤\")]"));
+                price = Float.toString(Float.parseFloat(text.getText().split("≤")[1].trim().split("\\)")[0]) - 100);
+            }
+        }
+        takeProfitPrice = price;
         return price;
+    }
+
+    public void fillInTextField(String textFieldName, String direction) {
+        if (driver instanceof AndroidDriver) {
+            switch (textFieldName) {
+                case "Stop Loss" -> stopLossTextFieldAos.sendKeys(getStopLossPrice(direction));
+                case "Take Profit" -> takeProfitTextFieldAos.sendKeys(getTakeProfitPrice(direction));
+                case "Lot Size" -> lotSizeTextFieldAos.sendKeys("0.45");
+            }
+        }
+    }
+
+    public void tapsButton(String buttonName) {
+        if (driver instanceof AndroidDriver) {
+            driver.findElement(By.xpath("(//android.widget.TextView[@text=\"" + buttonName + "\"])[2]/parent::android.view.ViewGroup")).click();
+        }
+    }
+
+    public String getConfirmationValue(String value) {
+        List<WebElement> text = driver.findElements(By.className("android.widget.TextView"));
+        for (int i = 0; i < text.size(); i++) {
+            if (text.get(i).getText().equalsIgnoreCase(value)) {
+                if (value.equalsIgnoreCase("Volume")) {
+                    return text.get(i + 1).getText().split("Lots")[0].trim();
+                }
+                return text.get(i + 1).getText();
+            }
+        }
+        return null;
+    }
+
+    public String getValidationValue(String label){
+        return switch (label) {
+            case "Stop Loss Price" -> String.format("%.2f",Double.parseDouble(stopLossPrice));
+            case "Take Profit Price" -> String.format("%.2f",Double.parseDouble(takeProfitPrice));
+            case "Direction" -> selectedDirection;
+            case "Volume" -> lotSize;
+            default -> null;
+        };
+    }
+
+    public void closeConfirmationPopUp(){
+        if (driver instanceof AndroidDriver) {
+            closeButtonAos.click();
+        }
+    }
+
+    public String getLotSize(){
+        return driver.findElement(By.className("android.widget.EditText")).getText();
+    }
+
+    public void getDirection(){
+        List<WebElement> direction = driver.findElements(By.xpath("//android.widget.FrameLayout[@resource-id=\"android:id/content\"]/android.widget.FrameLayout/android.view.ViewGroup" +
+                "/android.view.ViewGroup[3]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/" +
+                "android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup"));
+        for (WebElement element : direction) {
+            if (element.isSelected()) {
+            }
+        }
+    }
+
+    public void fillValueIntoTextField(String textFieldName,String value) throws InterruptedException {
+        if (driver instanceof AndroidDriver) {
+            switch (textFieldName) {
+                case "Lot Size" -> {
+                    driver.findElement(By.xpath("//android.widget.TextView[@text=\""+value+"\"]/parent::android.view.ViewGroup")).click();
+                    Thread.sleep(1000);
+                    lotSize = getLotSize();
+                }
+            }
+        }
+    }
+
+    public boolean getPositionDetail(String positionDetail){
+        for (WebElement element : rowsOnPositionTab) {
+            if (element.getText().equalsIgnoreCase(positionDetail)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public void getExecutedPrice(){
+        executedPrice = getConfirmationValue("Price");
+    }
+
+    public void tabCtaButton(String buttonName) {
+        if (driver instanceof AndroidDriver) {
+            switch (buttonName) {
+                case "Deposit detail" -> detailsButton.click();
+            }
+        }
     }
 
 
--- src/test/java/StepDefinitions/NativeApp/tradeSteps/tradeSteps.java
+++ src/test/java/StepDefinitions/NativeApp/tradeSteps/tradeSteps.java
@@ -1,10 +1,12 @@
 package StepDefinitions.NativeApp.tradeSteps;
 
 import PageObject.NativeApp.AppPOManager;
+import PageObject.NativeApp.AppTradeView;
 import io.cucumber.java.en.And;
 import StepDefinitions.NativeApp.login.loginSteps;
+import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.cucumber.java.en_scouse.An;
+import org.testng.Assert;
 
 public class tradeSteps {
 
@@ -18,12 +20,56 @@ public class tradeSteps {
     @And("the user selects direction {string} on the app trade view")
     public void the_user_selects_direction_on_the_app_trade_view(String direction) {
         appPoManager.getAppTradeView().selectDirection(direction);
-        System.out.println(appPoManager.getAppTradeView().getStopLossPrice());
-        System.out.println(appPoManager.getAppTradeView().getTakeProfitPrice());
+
     }
 
     @And("the user switches on take profit and stop loss on the app trade view")
-    public void the_user_switches_on_take_profit_and_stop_loss_on_the_app_trade_view(){
+    public void the_user_switches_on_take_profit_and_stop_loss_on_the_app_trade_view() throws InterruptedException {
+        System.out.println(appPoManager.getAppTradeView().getLotSize());
         appPoManager.getAppTradeView().switchProfitStopLoss();
+    }
+
+    @And("the user fills in the text field {string} with direction {string} on the app trade view")
+    public void the_user_fills_in_the_text_field_on_the_app_trade_view(String textFieldName,String direction) throws InterruptedException {
+        appPoManager.getAppTradeView().fillInTextField(textFieldName,direction);
+    }
+
+    @And("the user taps button {string} on the app trade view")
+    public void the_user_taps_button_buy_on_the_app_trade_view(String buttonName) throws InterruptedException {
+        Thread.sleep(200);
+        appPoManager.getAppTradeView().tapsButton(buttonName);
+    }
+
+    @Then("the user sees the value {string} is displayed correctly with the user input value on the confirmation pop up")
+    public void the_user_sees_the_value_is_displayed_correctly_with_the_user_input_value_on_the_confirmation_pop_up(String value) throws InterruptedException {
+        Thread.sleep(2000);
+        Assert.assertEquals(appPoManager.getAppTradeView().getConfirmationValue(value),
+                appPoManager.getAppTradeView().getValidationValue(value));
+        //appPoManager.getAppTradeView().closeConfirmationPopUp();
+    }
+
+    @And("the user fills in the text field {string} with value {string} on the app trade view")
+    public void the_user_fills_in_the_text_field_with_value_on_the_app_trade_view(String textFieldName,String value) throws InterruptedException {
+        appPoManager.getAppTradeView().fillValueIntoTextField(textFieldName,value);
+    }
+
+    @Then("the user sees a new open position is displayed at the position tab of instrument details page")
+    public void the_user_sees_a_new_open_position_is_displayed_at_the_position_tab_of_instrument_details_page() throws InterruptedException {
+        Thread.sleep(2000);
+        Assert.assertTrue(appPoManager.getAppTradeView().getPositionDetail(AppTradeView.executedPrice));
+        Assert.assertTrue(appPoManager.getAppTradeView().getPositionDetail(AppTradeView.selectedDirection));
+
+    }
+
+    @And("the user taps button {string} on the confirmation pop up")
+    public void the_user_taps_button_on_the_confirmation_pop_up(String buttonName) throws InterruptedException {
+        Thread.sleep(200);
+        appPoManager.getAppTradeView().getExecutedPrice();
+        appPoManager.getAppTradeView().tapsButton(buttonName);
+    }
+
+    @And("the user taps {string} cta button on the position tab of instrument details page")
+    public void the_user_taps_cta_button_on_the_position_tab_of_instrument_details_page(String buttonName){
+        appPoManager.getAppTradeView().tabCtaButton(buttonName);
     }
 }
